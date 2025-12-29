75f879370f5060c76d081e995ca1bec49dce39bb
Fortune <amoschegwe@gmail.com>
2025-12-30 00:21:45 +0100

    now upload

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 // Add this import at the very top of your App.jsx or index.js
-import './fonts.css';
+import './src/fonts.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
